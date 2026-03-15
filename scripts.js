c31bd6fb0336560ef04ec46cd387db302de37f5b
rbrasolin <rbgestaoempresarial@gmail.com>
2026-03-15 09:28:07 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,57 @@
+mermaid.initialize({ startOnLoad:false });
+
+function gerarFluxo(){
+
+let texto = document.getElementById("entrada").value;
+
+let linhas = texto.trim().split("\n");
+
+let mermaid = "flowchart TD\n";
+
+let nodes = {};
+let links = [];
+
+linhas.forEach(linha=>{
+
+let col = linha.split("\t");
+
+let id = col[1];
+let atividade = col[2];
+let sistema = col[4];
+let tempo = col[5];
+let proxSim = col[6];
+let proxNao = col[7];
+let cor = col[8] || "white";
+
+nodes[id] = `${id}["${atividade}<br>${sistema}<br>${tempo} min"]:::${cor}`;
+
+if(proxSim){
+links.push(`${id} --> ${proxSim}`);
+}
+
+if(proxNao){
+links.push(`${id} -->|Não| ${proxNao}`);
+}
+
+});
+
+Object.values(nodes).forEach(n=>{
+mermaid += n + "\n";
+});
+
+links.forEach(l=>{
+mermaid += l + "\n";
+});
+
+mermaid += `
+classDef blue fill:#8ecae6
+classDef yellow fill:#ffd166
+classDef green fill:#95d5b2
+`;
+
+document.getElementById("diagram").innerHTML =
+`<div class="mermaid">${mermaid}</div>`;
+
+mermaid.init();
+
+}
