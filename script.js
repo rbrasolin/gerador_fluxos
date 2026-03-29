b957383f106729a380d7ea639dfd774647013578
rbrasolin <rbgestaoempresarial@gmail.com>
2026-03-29 13:23:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const CONFIG = {
   decisionHeightFactor: 1.30,
   routeGap: 28,
   entryExitGap: 40,
-  laneGap: 28,
+  laneGap: 0, //antes28
   lanePaddingTop: 26,
   lanePaddingBottom: 26,
   laneLabelWidth: 70,
@@ -45,8 +45,8 @@ const EXCEL_LAYOUT = {
   colGap: 70, //Espaço horizontal entre as colunas (atividades)
   rowGap: 36, //Espaço vertical entre atividades dentro da mesma raia
   laneGap: 50, //Espaço entre uma raia e outra
-  lanePaddingTop: 25, //Espaço interno no topo da raia, Se estiver baixo → caixa “grudada” no topo
-  lanePaddingBottom: 25, //Espaço interno na parte de baixo da raia, Evita que última atividade fique colada na borda
+  lanePaddingTop: 40, //Espaço interno no topo da raia, Se estiver baixo → caixa “grudada” no topo
+  lanePaddingBottom: 40, //Espaço interno na parte de baixo da raia, Evita que última atividade fique colada na borda
   laneLabelWidth: 22, //“Reserva” horizontal para a área da raia (estrutura no desenho da raia, não aplica no excel)
   laneEntryWidth: 12, //Espaço entre: área do nome da raia e início do fluxo (Muito pequeno → fluxo começa “em cima” da raia)
   startGap: 16, //Distância entre: o “Início” e a primeira atividade
